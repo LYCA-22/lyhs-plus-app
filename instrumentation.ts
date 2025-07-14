3f6b32ca470e857d193300140c481716b9e7b5f0
Zhicheng Gong <109425to@gmail.com>
2025-07-14 20:05:49 +0800

    test-5

--- instrumentation.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-export async function register() {
-  await import("./sentry.edge.config");
-}
-
-export const onRequestError = Sentry.captureRequestError;
