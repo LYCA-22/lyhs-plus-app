3955ada71f7511ad5ea078c4190443c0394ae7f3
Zhicheng Gong <109425to@gmail.com>
2025-07-31 15:46:37 +0800

    fixed mail search

--- app/mail/view/page.tsx
+++ app/mail/view/page.tsx
@@ -2,12 +2,12 @@
 import { useEffect, useState } from "react";
 import { apiService } from "@/services/api";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+  Drawer,
+  DrawerContent,
+  DrawerTitle,
+  DrawerHeader,
+  DrawerDescription,
+} from "@/components/ui/drawer";
 import {
   InputOTP,
   InputOTPGroup,
@@ -123,161 +123,159 @@ export default function Page() {
         </div>
       </div>
 
-      <Dialog open={isOpen} onOpenChange={setIsOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>
-              <div className="flex flex-col">
-                <p className="text-lg">信件資訊</p>
-              </div>
-            </DialogTitle>
-            <DialogDescription>
-              {projectData ? (
-                <ul className="my-2 list-none w-full flex flex-col">
-                  <li className="bg-hoverbg rounded-lg relative rounded-bl-none rounded-br-none border-b border-borderColor">
-                    <p className="text-medium p-4 py-3 rounded-xl w-full bg-green-600 dark:bg-green-800 text-green-50 font-medium rounded-bl-none rounded-br-none">
-                      {projectData.status}
-                    </p>
-                    <div className="p-3 px-4">
-                      <p className="text-medium text-foreground font-medium">
-                        信件承辦人
-                      </p>
-                      <p className="text-foreground">
-                        {projectData.handler === "" ? (
-                          <div className="flex gap-2 mt-2">
-                            <div className="pt-1">
-                              <Info size={20} />
-                            </div>
-                            <div className="flex">
-                              <p>
-                                尚未有幹部承辦此信件，請稍作等待。若您的信件有任何更新時，我們會透過電子信箱聯絡您。
-                              </p>
-                            </div>
-                          </div>
-                        ) : (
-                          projectData.handler
-                        )}
-                      </p>
-                    </div>
-                  </li>
-                  <li className="w-full flex-col bg-hoverbg rounded-tl-none rounded-tr-none text-foreground text-medium font-medium p-4 flex justify-center rounded-xl relative">
-                    <p className="text-medium text-foreground font-medium">
-                      信件資訊
-                    </p>
-                    <div className="flex gap-2 items-center mt-2">
-                      <RectangleEllipsis size={20} />
-                      <div className="flex items-center gap-2">
-                        <p className="text-sm text-foreground font-medium">
-                          信件專用代碼
-                        </p>
-                        <p className="text-sm text-inputPrimary font-bold">
-                          {projectData.searchCode}
-                        </p>
+      <Drawer open={isOpen} onOpenChange={setIsOpen}>
+        <DrawerContent className="max-h-screen-56 overflow-y-auto p-5 mx-2">
+          <DrawerTitle>
+            <div className="flex flex-col">
+              <p className="text-lg">信件資訊</p>
+            </div>
+          </DrawerTitle>
+          {projectData ? (
+            <ul className="my-2 list-none w-full flex flex-col">
+              <li className="bg-hoverbg rounded-lg relative rounded-bl-none rounded-br-none border-b border-borderColor">
+                <p className="text-medium p-4 py-3 rounded-xl w-full bg-green-600 dark:bg-green-800 text-green-50 font-medium rounded-bl-none rounded-br-none">
+                  {projectData.status}
+                </p>
+                <div className="p-3 px-4">
+                  <p className="text-medium text-foreground font-medium">
+                    信件承辦人
+                  </p>
+                  <p className="text-foreground">
+                    {projectData.handler === "" ? (
+                      <div className="flex gap-2 mt-2">
+                        <div className="pt-1">
+                          <Info size={20} />
+                        </div>
+                        <div className="flex">
+                          <p>
+                            尚未有幹部承辦此信件，請稍作等待。若您的信件有任何更新時，我們會透過電子信箱聯絡您。
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                    <p className="font-normal text-xs text-foreground opacity-60 ml-7">
-                      請務必記得代碼，以讓你可以追蹤此信件。
+                    ) : (
+                      projectData.handler
+                    )}
+                  </p>
+                </div>
+              </li>
+              <li className="w-full flex-col bg-hoverbg rounded-tl-none rounded-tr-none text-foreground text-medium font-medium p-4 flex justify-center rounded-xl relative">
+                <p className="text-medium text-foreground font-medium">
+                  信件資訊
+                </p>
+                <div className="flex gap-2 items-center mt-2">
+                  <RectangleEllipsis size={20} />
+                  <div className="flex items-center gap-2">
+                    <p className="text-sm text-foreground font-medium">
+                      信件專用代碼
                     </p>
-                    <button
-                      onClick={handleCopyCode}
-                      className="flex items-center p-2 rounded-lg
+                    <p className="text-sm text-inputPrimary font-bold">
+                      {projectData.searchCode}
+                    </p>
+                  </div>
+                </div>
+                <p className="font-normal text-xs text-foreground opacity-60 ml-7">
+                  請務必記得代碼，以讓你可以追蹤此信件。
+                </p>
+                <button
+                  onClick={handleCopyCode}
+                  className="flex items-center p-2 rounded-lg
                                  text-sm text-foreground hover:bg-background transition-colors absolute right-5"
-                    >
-                      {copied ? icons["copyDone"]() : <Copy size={20} />}
-                    </button>
-                  </li>
-                  <li className="mt-6 text-white rounded-xl flex-col gap-2 bg-gradient-to-br from-inputPrimary to-background p-4 pb-5">
-                    <div className="flex gap-2 items-center">
-                      <ShieldCheck size={25} />
-                      <p className="font-bold text-lg">
-                        您的信件正安全的被我們保護。
-                      </p>
-                    </div>
-                    <button className="rounded-lg border text-white border-white p-2 px-3 mt-2 ml-8 hover:opacity-55 text-sm flex gap-2 items-center">
-                      了解我們怎麼保護你的資料
-                      <SquareArrowOutUpRight size={15} />
-                    </button>
-                  </li>
-                  <li className="mt-6 text-foreground border border-hoverbg rounded-xl pb-2">
-                    <p className="text-medium text-foreground font-medium m-4 flex gap-2 items-center">
-                      <FileText size={20} />
-                      寄件者資料
-                    </p>
-                    <div className="flex items-center mx-4">
-                      <p className="text-sm mr-2 min-w-[70px]">姓名</p>
-                      <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
-                        {projectData.name}
-                      </p>
-                    </div>
-                    <div className="flex items-center mx-4">
-                      <p className="text-sm mr-2 min-w-[70px]">電子郵件</p>
-                      <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
-                        {projectData.email}
-                      </p>
-                    </div>
-                    <div className="flex items-center mx-4">
-                      <p className="text-sm mr-2 min-w-[70px]">班級</p>
-                      <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
-                        {projectData.class}
-                      </p>
-                    </div>
-                    <div className="flex items-center mx-4">
-                      <p className="text-sm mr-2 min-w-[70px]">座號</p>
-                      <p className="text-sm font-normal flex grow justify-end py-2">
-                        {projectData.number}
-                      </p>
-                    </div>
-                  </li>
-                  <li className="text-xl mt-6 font-bold text-foreground border border-hoverbg rounded-xl pb-2">
-                    <p className="text-medium text-foreground font-medium m-4 flex gap-2 items-center">
-                      <Mail size={20} />
-                      信件內容
-                    </p>
-                    <div className="flex items-center mx-4">
-                      <p className="text-sm mr-2 min-w-[70px]">大綱</p>
-                      <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
-                        {projectData.title}
-                      </p>
-                    </div>
-                    <div className="flex items-center mx-4">
-                      <p className="text-sm mr-2 min-w-[70px]">說明</p>
-                      <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
-                        {projectData.description}
-                      </p>
-                    </div>
-                    <div className="flex items-center mx-4">
-                      <p className="text-sm mr-2 min-w-[70px]">解決方式</p>
-                      <p className="text-sm font-normal flex grow justify-end py-2">
-                        {projectData.solution}
-                      </p>
-                    </div>
-                  </li>
-                  <li className="mt-6 flex max-sm:flex-col gap-4">
-                    <button
-                      className="bg-foreground text-background w-full rounded-xl p-3 px-4 flex items-center justify-center hover:opacity-75 font-medium"
-                      onClick={() => {
-                        setIsOpen(false);
-                      }}
-                    >
-                      關閉
-                    </button>
-                    <button
-                      className="bg-buttonBg text-foreground w-full rounded-xl p-3 px-4 flex items-center justify-center hover:opacity-75 font-medium"
-                      onClick={() => {
-                        setIsOpen(false);
-                      }}
-                    >
-                      錯誤回報
-                    </button>
-                  </li>
-                </ul>
-              ) : (
-                <p>無法載入案件資料</p>
-              )}
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+                >
+                  {copied ? icons["copyDone"]() : <Copy size={20} />}
+                </button>
+              </li>
+              <li className="mt-6 text-white rounded-xl flex-col gap-2 bg-gradient-to-br from-inputPrimary to-background p-4">
+                <div className="flex gap-2 items-center">
+                  <ShieldCheck size={25} />
+                  <p className="font-bold text-[14px]">
+                    您的信件正安全的被我們保護。
+                  </p>
+                </div>
+                {/*
+                <button className="rounded-lg border text-white border-white p-2 px-3 mt-2 ml-8 hover:opacity-55 text-sm flex gap-2 items-center">
+                  了解我們怎麼保護你的資料
+                  <SquareArrowOutUpRight size={15} />
+                </button>
+                */}
+              </li>
+              <li className="mt-6 text-foreground border border-hoverbg rounded-xl pb-2">
+                <p className="text-medium text-foreground font-medium m-4 flex gap-2 items-center">
+                  <FileText size={20} />
+                  寄件者資料
+                </p>
+                <div className="flex items-center mx-4">
+                  <p className="text-sm mr-2 min-w-[70px]">姓名</p>
+                  <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
+                    {projectData.name}
+                  </p>
+                </div>
+                <div className="flex items-center mx-4">
+                  <p className="text-sm mr-2 min-w-[70px]">電子郵件</p>
+                  <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
+                    {projectData.email}
+                  </p>
+                </div>
+                <div className="flex items-center mx-4">
+                  <p className="text-sm mr-2 min-w-[70px]">班級</p>
+                  <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
+                    {projectData.class}
+                  </p>
+                </div>
+                <div className="flex items-center mx-4">
+                  <p className="text-sm mr-2 min-w-[70px]">座號</p>
+                  <p className="text-sm font-normal flex grow justify-end py-2">
+                    {projectData.number}
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6 font-bold text-foreground border border-hoverbg rounded-xl pb-2">
+                <p className="text-foreground font-medium m-4 flex gap-2 items-center">
+                  <Mail size={20} />
+                  信件內容
+                </p>
+                <div className="flex items-center mx-4">
+                  <p className="text-sm mr-2 min-w-[70px]">大綱</p>
+                  <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
+                    {projectData.title}
+                  </p>
+                </div>
+                <div className="flex items-center mx-4">
+                  <p className="text-sm mr-2 min-w-[70px]">說明</p>
+                  <p className="text-sm font-normal flex grow justify-end py-2 border-b border-border">
+                    {projectData.description}
+                  </p>
+                </div>
+                <div className="flex items-center mx-4">
+                  <p className="text-sm mr-2 min-w-[70px]">解決方式</p>
+                  <p className="text-sm font-normal flex grow justify-end py-2">
+                    {projectData.solution}
+                  </p>
+                </div>
+              </li>
+              <li className="mt-6 flex max-sm:flex-col gap-4">
+                <button
+                  className="bg-foreground text-background w-full rounded-xl p-3 px-4 flex items-center justify-center hover:opacity-75 font-medium"
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  關閉
+                </button>
+                <button
+                  className="bg-buttonBg text-foreground w-full rounded-xl p-3 px-4 flex items-center justify-center hover:opacity-75 font-medium"
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  錯誤回報
+                </button>
+              </li>
+            </ul>
+          ) : (
+            <p>無法載入案件資料</p>
+          )}
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 }
