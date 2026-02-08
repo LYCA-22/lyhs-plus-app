3f005beecf997f2b2f8b73e2ae20aa440111db82
Zhicheng Gong <109425to@gmail.com>
2026-02-08 22:41:33 +0800

    update ai chat ui/ux

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -24,6 +24,7 @@ export default function ChatPage() {
       setAiResponseHistory((prev) => [...prev, response.text]);
       setUserInput("");
     } catch (e) {
+      setAiResponseHistory((prev) => [...prev, "伺服器發生錯誤，請重試。"]);
       console.error(e);
     } finally {
       dispatch(updatePageLoadingStatus(false));
@@ -31,7 +32,9 @@ export default function ChatPage() {
   };
 
   return (
-    <div className="flex flex-col min-h-dvh">
+    <div
+      className={`flex flex-col h-dvh ${appData.device_info.operate_type === "PWA" ? "pb-32" : "pb-24"}`}
+    >
       <div className="p-5">
         <div className="flex items-center gap-2">
           <h1 className="text-2xl font-medium">AI 聊天</h1>
@@ -39,9 +42,11 @@ export default function ChatPage() {
             BETA
           </p>
         </div>
-        <p className="opacity-50">此功能可以協助您找到學校的相關資訊。</p>
+        <p className="opacity-50 mt-1">
+          此功能可以協助您找到學校的相關資訊，您的對話紀錄將不會保留。
+        </p>
       </div>
-      <div className="px-5 mt-3 space-y-5">
+      <div className="px-5 mt-3 space-y-5 overflow-y-auto pb-5 grow">
         {userInputHistory.map((input, index) => (
           <div key={index} className="flex flex-col w-full gap-5">
             <div className="bg-sky-100 rounded-full ml-auto p-2 px-3">
@@ -51,9 +56,7 @@ export default function ChatPage() {
           </div>
         ))}
       </div>
-      <div
-        className={`fixed w-full left-0 px-5 flex justify-center flex-col ${appData.device_info.operate_type === "PWA" ? "bottom-[104px]" : "bottom-24"}`}
-      >
+      <div className={`w-full px-5 flex justify-center flex-col relative`}>
         <Input
           placeholder="在這裡輸入你的問題..."
           className="bg-background border border-border p-4 rounded-2xl shadow-md"
@@ -62,7 +65,7 @@ export default function ChatPage() {
         />
         <button
           onClick={() => handleAiRes()}
-          className="bg-sky-600 text-white p-2 rounded-xl absolute bottom-[38px] right-[32px]"
+          className="bg-sky-600 text-white p-2 rounded-xl absolute top-3 right-[32px]"
         >
           <SendHorizontal size={20} />
         </button>
